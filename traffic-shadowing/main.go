0a63db9d7bf25a392d9250cddc4b2b23b6a98848
shikanon <shikanon@tensorbytes.com>
2021-02-24 17:49:47 +0800

    添加hello world 案例

---
+++ traffic-shadowing/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"strings"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":8000"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	log.Println("start!")
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		log.Println("Key: ", pair[0])
+		log.Println("Value: ", pair[1])
+	}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
